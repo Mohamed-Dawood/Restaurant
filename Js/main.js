329faac1211728c773edd7c2f6a92dfe5e877b4f
Mohamed Dawood <mohamedbasyonidawood23@gmail.com>
2025-07-27 10:58:57 +0300

    modify pages linkds

--- Js/main.js
+++ Js/main.js
@@ -5,10 +5,10 @@ let passwordInput = document.querySelector("[name='password']");
 document.onclick = function (e) {
   let userValid = false;
   let passwordValid = false;
-  if (userInput !== "" && userInput.value.length >= 10) {
+  if (userInput !== '' && userInput.value.length >= 10) {
     userValid = true;
   }
-  if (passwordInput !== "" && passwordInput.value.length >= 7) {
+  if (passwordInput !== '' && passwordInput.value.length >= 7) {
     passwordValid = true;
   }
   if (userValid === false || passwordValid === false) {
@@ -16,19 +16,19 @@ document.onclick = function (e) {
   }
 };
 
-let btn = document.getElementById("btn-icon");
-let uln = document.getElementById("uln");
+let btn = document.getElementById('btn-icon');
+let uln = document.getElementById('uln');
 // let home = document.getElementById("home");
 console.log(uln);
 console.log(btn);
 
-btn.addEventListener("click", function () {
-  uln.classList.toggle("toggle");
-  btn.classList.toggle("toggle-i");
-  console.log("work");
+btn.addEventListener('click', function () {
+  uln.classList.toggle('toggle');
+  btn.classList.toggle('toggle-i');
+  console.log('work');
   console.log(uln);
 });
-let btn2 = document.getElementById("btn2");
+let btn2 = document.getElementById('btn2');
 
 window.onscroll = function () {
   if (scrollY >= 400) {
@@ -42,12 +42,12 @@ btn2.onclick = function () {
   scroll({
     left: 0,
     top: 0,
-    behavior: "smooth",
+    behavior: 'smooth',
   });
 };
-let exploreMenu = document.querySelector(".explore-menu");
+let exploreMenu = document.querySelector('.explore-menu');
 
 exploreMenu.addEventListener(
-  "click",
-  () => (window.location.href = "http://127.0.0.1:5500/Filter.html")
+  'click',
+  () => (window.location.href = 'Filter.html')
 );
